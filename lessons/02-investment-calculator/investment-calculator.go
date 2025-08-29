fb4f7cf027bca7c6edb626a812775881e4e0a87f
Jim <jimbankshome@gmail.com>
2025-08-28 23:02:18 -0600

    Collect user input for values and also calc and output inflation adjusted real value

--- lessons/02-investment-calculator/investment-calculator.go
+++ lessons/02-investment-calculator/investment-calculator.go
@@ -1,20 +1,40 @@
 package main
 
-import "fmt"
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	
+	// Declare main() constants
+	const InflationRate    float64 = 2.67
+
 	// Declare main() variables
-	var investmentAmount float64 = 500000.00
-	var expectedReturnRate float64 = 8.5
-	var futureValue float64 = 0.0
-	var yearsInvested float64 = 10
+	var investmentAmount   float64 = 0
+	var expectedReturnRate float64 = 0
+	var futureRealValue    float64 = 0
+	var futureValue        float64 = 0
+	var yearsInvested      float64 = 0
+
+    // Enter values to calculate
+    fmt.Print("Input investment amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Input expected return rate percentage: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Input years of expected investment: ")
+	fmt.Scan(&yearsInvested)
 
     // Calculate future value
-	futureValue = investmentAmount * math.Pow((1 + (expectedReturnRate / 100)), yearsInvested) 
+	futureValue = investmentAmount * math.Pow(1 + (expectedReturnRate / 100), yearsInvested) 
 
-	// Output future value
+	// Calculate the future real value (inflation adjusted)
+	futureRealValue = futureValue / math.Pow(1 + (InflationRate/100), yearsInvested)
+
+	// Output future and real values
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
  
