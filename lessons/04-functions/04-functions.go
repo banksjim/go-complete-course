88d3afec57821b53740e561f183d23804fc26267
Jim Banks <jimbankshome@gmail.com>
2025-09-16 17:07:18 -0600

    Basic function example

--- lessons/04-functions/04-functions.go
+++ lessons/04-functions/04-functions.go
@@ -3,6 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Lesson: 04-functions")
-	// Add your code here
+    text1 := ""
+	text2 := ""
+
+	text1 = "Hello\n"
+	text2 = "World!\n"	
+
+	outputText(text1, text2)
+}
+
+func outputText(textString1 string, textString2 string) {
+	fmt.Print(textString1)
+	fmt.Print(textString2)
 }
