cd2d4ee053b02be60cd9d5a0df6df3954679861f
Jim Banks <jimbankshome@gmail.com>
2025-11-10 15:45:35 -0700

    Refactor app to use switch for menu

--- lessons/09-switch/bank.go
+++ lessons/09-switch/bank.go
@@ -11,80 +11,85 @@ func main() {
 
 	// Present welcome message
 	fmt.Println("\nWelcome to Go Bank!")
-	fmt.Println("What do you want to do?")
-
-	// Present app options
-	fmt.Println("\n1. Check balance")
-	fmt.Println("2. Deposit money")
-	fmt.Println("3. Withdraw money")
-	fmt.Println("4. Exit")
 
     // Loop until user selects the Exit option
 	for {
+
+		// Present app options
+		fmt.Println("\nWhat do you want to do?")
+		fmt.Println("\n1. Check balance")
+		fmt.Println("2. Deposit money")
+		fmt.Println("3. Withdraw money")
+		fmt.Println("4. Exit")
 
 		// Select user options
 		fmt.Print("\nSelection: ")
 		fmt.Scan(&userSelection)
 
-		// Handle user options
-		if userSelection >= 1 && userSelection <= 4 {
-			
-			if userSelection == 1 { // Check balance
-				
-				// Print account balance
-				fmt.Printf("Account balance: %.2f\n", accountBalance)
-			
-			} else if userSelection == 2 { // Deposit money
+		switch userSelection {
 
-				// Enter deposit amount
-				fmt.Print("Deposit amount: ")
-				fmt.Scan(&userAmount)
+		case 1:
 
-				// Ensure deposit amount is > 0
-				if userAmount > 0 {
+			// Print account balance
+			fmt.Printf("Account balance: %.2f\n", accountBalance)
 
-					// Add deposit amount to balance
-					accountBalance += userAmount
+		case 2:
 
-				} else {
-					
-					fmt.Println("Invalid amount. Must be greater than zero.")
-					continue
+			// Enter deposit amount
+			fmt.Print("Deposit amount: ")
+			fmt.Scan(&userAmount)
 
-				} 
+            // Validation guard checks:
+			// Check: Deposit amount greater than 0
+			if userAmount <= 0 {
 
-			} else if userSelection == 3 { // Withdraw money
-
-				// Enter withdrawal amount
-				fmt.Print("Withdrawal amount: ")
-				fmt.Scan(&userAmount)
-
-                if userAmount <= accountBalance {
-
-                // Subtract withdrawal amount from balance
-				accountBalance -= userAmount
-
-				} else {
-
-                    fmt.Println("Invalid amount. Must be less than or equal to account balance.")
-					continue
-
-				}
-
-			} else if userSelection == 4 { // Exit - Terminate the app
-
-				break;
+                fmt.Println("Invalid amount. Must be greater than zero.")
+				continue
 
 			}
 
-		} else {
-			
+            // Add deposit amount to balance
+			accountBalance += userAmount
+
+		case 3:
+
+			// Enter withdrawal amount
+			fmt.Print("Withdrawal amount: ")
+			fmt.Scan(&userAmount)
+
+            // Validation guard checks:
+			// Check: Withdrawal amount must be greater than zero
+			if userAmount <= 0 {
+
+                fmt.Println("Invalid amount. Must be less than or equal to account balance.")
+				continue
+
+			}
+
+            // Check: Withdrawal amount must be less than or equal balance
+			if userAmount > accountBalance {
+
+                fmt.Println("Invalid amount. Must be less than or equal to account balance.")
+				continue
+
+			}
+
+			// Subtract withdrawal amount from balance
+			accountBalance -= userAmount
+
+		case 4:
+
+	        // Exit the main function
+			fmt.Println("Thanks for choosing our bank!")
+			return
+
+		default:
+
+			// Error: User did not select a valid menu option
 			fmt.Println("**Input error**")
 
 		}
 
 	}
-
-	fmt.Println("Thanks for choosing our bank!")
 
 }
